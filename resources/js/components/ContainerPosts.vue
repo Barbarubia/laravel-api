0ad2fc66e7e02d3b39e8732d982e807222704536
Barbarubia <dnl.desimone@gmail.com>
2022-05-26 15:02:23 +0200

    Creata app Vue + component

---
+++ resources/js/components/ContainerPosts.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Posts
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ContainerPosts',
+}
+</script>
+
+<style>
+
+</style>
---
+++ resources/js/views/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <container-posts />
+    </div>
+</template>
+
+<script>
+import ContainerPosts from  '../components/ContainerPosts.vue';
+export default {
+    name: 'App',
+    components: {
+        ContainerPosts,
+    }
+}
+</script>
+
+<style>
+
+</style>
